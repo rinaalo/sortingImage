2880600527f83db949ddca69264f34f19bae0545
alor <93737038+rinaalo@users.noreply.github.com>
2023-10-07 20:08:42 +0200

    - added insertion sort - added stbi_write functions

--- src/main.c
+++ src/main.c
@@ -7,6 +7,9 @@
 #include "stb_image_write.h"
 
 #define IMAGE "img/nisa.jpg"
+#define OUTPUT_IMAGE "output/nisa"
+//#define IMAGE "img/otter.png"
+//#define OUTPUT_IMAGE "output/otter"
 
 typedef unsigned char u8;
 
@@ -16,45 +19,56 @@ typedef struct {
     u8 G;
 } Color;
 
-float luminance_of_pixel(u8 *pixel) {
-    // assuming rgb order
+float luminance_of_pixel(Color pixel) {
     // luminance formula: https://en.wikipedia.org/wiki/Relative_luminance
-    float R = pixel[0], G = pixel[1], B = pixel[2];
-    return 0.2126*R + 0.7152*G + 0.0722*B;
+    return pixel.R + pixel.G + pixel.B;
+    // return 0.2126*pixel.R + 0.7152*pixel.G + 0.0722*pixel.B;
 }
 
-void sort_column_by_luminance(u8 *pixel) {
+void sort_column_by_luminance(Color *column, int length) {
     // assuming row major
+    // using insertion sort
+    for (int i = 1; i < length; i++) {
+        Color x = column[i];
+        int j = i - 1;
+        while (j >= 0 && luminance_of_pixel(column[j]) > luminance_of_pixel(x)) {
+            column[j + 1] = column[j];
+            j--;
+        }
+        column[j + 1] = x;
+    }
 }
 
 int main() {
     int width, height, channels;
-    u8 *img = stbi_load(IMAGE, &width, &height, &channels, 0);
+    Color *img = (Color*)stbi_load(IMAGE, &width, &height, &channels, 0);
     if (img == NULL) {
-        printf("Error loading the image\n");
+        fprintf(stderr,"Error loading the image\n");
         exit(1);
     }
-    printf("image w:%d h:%d ch:%d\n", width, height, channels);
-    Color *col_img = (Color*) img; 
 
-    // sort image
-    Color *column_buffer = malloc(height);
+    Color *column_buffer = malloc(height * sizeof(Color));
     for (int column = 0; column < width; column++) {
-        // copy column into buffer
         for (int row = 0; row < height; row++) {
-            column_buffer[row] = col_img[column + row * width];
-            //printf("r %X g %X b %X\n", column_buffer[row].R, column_buffer[row].B, column_buffer[row].G);
+            column_buffer[row] = img[column + row * width];
         }
-        // sort buffer
-        // copy buffer into column
+        sort_column_by_luminance(column_buffer, height);
+        for (int row = 0; row < height; row++) {
+            img[column + row * width] = column_buffer[row];
+        }
     }
     free(column_buffer);
-    stbi_image_free(img);
     // create new image out of image buffer
-
-    // calculate brightness of given pixel
-    // sorting algorithm
-    // run it every column 
-    // output resulting image
+    //int aa = stbi_write_jpg(OUTPUT_IMAGE, width, height, channels, img, 100);
+    if (!stbi_write_jpg(OUTPUT_IMAGE ".jpg", width, height, channels, img, 100)) {
+        fprintf(stderr, "Could not create image %s.jpg", OUTPUT_IMAGE);
+    }
+    if (!stbi_write_bmp(OUTPUT_IMAGE ".bmp", width, height, channels, img)) {
+        fprintf(stderr, "Could not create image %s.bmp", OUTPUT_IMAGE);
+    }
+    if (!stbi_write_png(OUTPUT_IMAGE ".png", width, height, channels, img, width * channels)) {
+        fprintf(stderr, "Could not create image %s.png", OUTPUT_IMAGE);
+    }
+    stbi_image_free(img);
     return 0;
 }
