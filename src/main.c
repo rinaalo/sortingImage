35bf494ed71f86506b9bf6e92dc62f2119f55b09
alor <93737038+rinaalo@users.noreply.github.com>
2023-10-07 23:49:22 +0200

    - added vertical sort - added hue evaluation

--- src/main.c
+++ src/main.c
@@ -6,32 +6,46 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-#define IMAGE "img/nisa.jpg"
-#define OUTPUT_IMAGE "output/nisa"
-//#define IMAGE "img/otter.png"
-//#define OUTPUT_IMAGE "output/otter"
+//#define IMAGE "img/nisa.jpg"
+//#define OUTPUT_IMAGE "output/nisa"
+#define IMAGE "img/otter.png"
+#define OUTPUT_IMAGE "output/otter"
+
+#define MAX(X,Y) (X>Y) ? X:Y
+#define MIN(X,Y) (X<Y) ? X:Y
 
 typedef unsigned char u8;
-
 typedef struct {
     u8 R;
     u8 B;
     u8 G;
 } Color;
+typedef float (*pixel_evaluator) (Color);
 
 float luminance_of_pixel(Color pixel) {
     // luminance formula: https://en.wikipedia.org/wiki/Relative_luminance
-    return pixel.R + pixel.G + pixel.B;
-    // return 0.2126*pixel.R + 0.7152*pixel.G + 0.0722*pixel.B;
+    return 0.2126*pixel.R + 0.7152*pixel.G + 0.0722*pixel.B;
+}
+float hue_of_pixel(Color pixel) {
+    // hue formula: https://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/
+    float R = (float)pixel.R / 255.0;
+    float G = (float)pixel.G / 255.0;
+    float B = (float)pixel.B / 255.0;
+    
+    float max = MAX(R, MAX(G, B));
+    float min = MIN(R, MIN(G, B));
+
+    if(max == R)        return (G - B) / (max - min);
+    else if(max == G)   return 2.0 + (B - R) / (max - min);
+    else                return 4.0 + (R - G) / (max - min);
 }
 
-void sort_column_by_luminance(Color *column, int length) {
-    // assuming row major
+void sort_buffer(Color *column, int length, pixel_evaluator eval) {
     // using insertion sort
     for (int i = 1; i < length; i++) {
         Color x = column[i];
         int j = i - 1;
-        while (j >= 0 && luminance_of_pixel(column[j]) > luminance_of_pixel(x)) {
+        while (j >= 0 && eval(column[j]) > eval(x)) {
             column[j + 1] = column[j];
             j--;
         }
@@ -39,27 +53,45 @@ void sort_column_by_luminance(Color *column, int length) {
     }
 }
 
+void sort_image_vertically(Color *img, int width, int height, pixel_evaluator eval) {
+    Color *column_buffer = malloc(height * sizeof(Color));
+    for (int column = 0; column < width; column++) {
+        for (int row = 0; row < height; row++) {
+            column_buffer[row] = img[column + row * width];
+        }
+        sort_buffer(column_buffer, height, eval);
+        for (int row = 0; row < height; row++) {
+            img[column + row * width] = column_buffer[row];
+        }
+    }
+    free(column_buffer);
+}
+
+void sort_image_horizontally(Color *img, int width, int height, pixel_evaluator eval) {
+    Color *row_buffer = malloc(width * sizeof(Color));
+    for (int row = 0; row < height; row++) {
+        for (int column = 0; column < width; column++) {
+            row_buffer[column] = img[column + row * width];
+        }
+        sort_buffer(row_buffer, width, eval);
+        for (int column = 0; column < width; column++) {
+            img[column + row * width] = row_buffer[column];
+        }
+    }
+    free(row_buffer);
+}
+
 int main() {
+    printf("%zu\n", sizeof(Color));
     int width, height, channels;
     Color *img = (Color*)stbi_load(IMAGE, &width, &height, &channels, 0);
     if (img == NULL) {
         fprintf(stderr,"Error loading the image\n");
         exit(1);
     }
+    //sort_image_vertically(img, width, height, &hue_of_pixel);
+    sort_image_horizontally(img, width, height, &luminance_of_pixel);
 
-    Color *column_buffer = malloc(height * sizeof(Color));
-    for (int column = 0; column < width; column++) {
-        for (int row = 0; row < height; row++) {
-            column_buffer[row] = img[column + row * width];
-        }
-        sort_column_by_luminance(column_buffer, height);
-        for (int row = 0; row < height; row++) {
-            img[column + row * width] = column_buffer[row];
-        }
-    }
-    free(column_buffer);
-    // create new image out of image buffer
-    //int aa = stbi_write_jpg(OUTPUT_IMAGE, width, height, channels, img, 100);
     if (!stbi_write_jpg(OUTPUT_IMAGE ".jpg", width, height, channels, img, 100)) {
         fprintf(stderr, "Could not create image %s.jpg", OUTPUT_IMAGE);
     }
